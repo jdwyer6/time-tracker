1484ea9f308b432ccfa2e63fbea76e71157ee76c
kegcheck <65734212+kegcheck@users.noreply.github.com>
2022-11-01 21:46:36 -0500

    set up login and register

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,20 +2,62 @@ import { Form } from 'react-bootstrap';
 import {BiUserCircle} from 'react-icons/bi';
 import { RiLockPasswordLine } from 'react-icons/ri';
 import { MdOutlineLogin } from 'react-icons/md';
+import { useState } from 'react';
+import Axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [errMsg, setErrMsg] = useState(false);
+    let navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false)
+
+    const config = {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+        }
+    }
+
+    const login = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        Axios.post("https://clockedin.herokuapp.com/login", {username, password}, config)
+        .then((res) => {
+            if(res.status === 200){
+                // dispatch(setUser(JSON.stringify(res.data)));
+                localStorage.setItem('currentUser', JSON.stringify(res.data))
+                setLoading(false);
+                navigate('/profile');
+            }
+        })
+        .catch(error => {
+            console.log(error.response)
+            setErrMsg(true);
+        })
+    }
+
+    if(isLoading){
+        return(
+            <h1>Loading...</h1>
+        )
+    }
+
+
     return ( 
         <div className='form-container'>
             <h2>Log in</h2>
             <p>Enter your details below to continue</p>
-            <form onSubmit=''>
+            <form onSubmit={login}>
                 <div className='d-flex align-items-center my-4'>
                     <BiUserCircle />
-                    <Form.Control type='text' id='username' name='username' placeholder='Username' className='border-0 border-bottom'/>
+                    <Form.Control type='text' id='username' name='username' placeholder='Username' className='border-0 border-bottom' onChange={(e)=>{setUsername(e.target.value)}}/>
                 </div>
                 <div className='d-flex align-items-center my-4'>
                     <RiLockPasswordLine />
-                    <Form.Control type='text' id='password' name='password' placeholder='Password' className='border-0 border-bottom'/>
+                    <Form.Control type='text' id='password' name='password' placeholder='Password' className='border-0 border-bottom' onChange={(e)=>{setPassword(e.target.value)}}/>
                 </div>
                 <button className='btn-primary' type='submit'>Log in <MdOutlineLogin /></button>
                 <div className='d-flex justify-content-end py-0 my-0'>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,55 @@
 import { useState } from 'react';
 import {Button, Form} from 'react-bootstrap';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { useNavigate } from 'react-router-dom';
+import { userSchema } from '../Validations/UserValidation'; 
+import Axios from 'axios';
 
 const Register = ({handleClose, handleShow, show}) => {
+
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ name, setName ] = useState('')
+    const [ businessName, setBusinessName ] = useState('');
+    const [ admin, setAdmin ] = useState(true);
+    const [valid, setValid] = useState(false);
+    const [isLoading, setLoading] = useState(false);
+    let navigate = useNavigate();
+
+
+    const validateInfo = async (e) =>{
+        let formData = {
+            username: e.target[0].value,
+            password: e.target[1].value,
+            businessName: e.target[3].value
+        };
+        const isValid = await userSchema.isValid(formData)
+        userSchema.validate(formData)
+        .catch(function(err){
+            alert(`${err.name} \n ${err.errors}`)
+        })
+        isValid ? setValid(true) : setValid(false)
+    }
+
+
+    const createUser = (e) => {
+        e.preventDefault();
+        console.log(username, password, name, businessName, admin)
+        validateInfo(e)
+        if(valid){
+            setLoading(true)
+            Axios.post("https://clockedin.herokuapp.com/register", {username, password, name, businessName, admin})    
+            .then((response) => {
+                alert('SUCCESS! New user created!')
+                setLoading(false);
+                navigate('/login');
+            })
+            .catch(error => {
+                console.log(error.response)
+                alert('Username already taken')
+            })
+        }
+    }
 
     return ( 
         <>
@@ -12,25 +59,25 @@ const Register = ({handleClose, handleShow, show}) => {
                 <Offcanvas.Title>Register your business</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-                <form onSubmit=''>
+                <Form onSubmit={createUser}>
                     <div className='d-flex align-items-center my-4'>
-                        <Form.Control type='text' id='username' name='username' placeholder='Create a username' className='border-0 border-bottom'/>
+                        <Form.Control type='text' id='username' name='username' placeholder='Create a username' className='border-0 border-bottom' onChange={(e)=>{setUsername(e.target.value)}}/>
                     </div>
                     <div className='d-flex align-items-center my-4'>
-                        <Form.Control type='text' id='password' name='password' placeholder='Create a password' className='border-0 border-bottom'/>
+                        <Form.Control type='password' id='password' name='password' placeholder='Create a password' className='border-0 border-bottom' onChange={(e)=>{setPassword(e.target.value)}}/>
                     </div>
                     <div className='d-flex align-items-center my-4'>
-                        <Form.Control type='text' id='name' name='name' placeholder='Enter your name' className='border-0 border-bottom'/>
+                        <Form.Control type='text' id='name' name='name' placeholder='Enter your name' className='border-0 border-bottom' onChange={(e)=>{setName(e.target.value)}}/>
                     </div>
                     <div className='d-flex align-items-center my-4'>
-                        <Form.Control type='text' id='name' name='business' placeholder='Enter the name of your business' className='border-0 border-bottom'/>
+                        <Form.Control type='text' id='business' name='business' placeholder='Enter the name of your business' className='border-0 border-bottom' onChange={(e)=>{setBusinessName(e.target.value)}}/>
                     </div>
                     <button className='btn-primary' type='submit'>Register</button>
                     <div className='d-flex justify-content-end py-0 my-0'>
                         <p className='font-small__blue'>Need help?</p>
                     </div>
 
-                </form>
+                </Form>
             </Offcanvas.Body>
             </Offcanvas>
         </>
